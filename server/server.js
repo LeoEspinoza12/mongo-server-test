1eef2382833f6c8c79e29928141d7f263461b4d9
Leo espinoza <leo.spinoza@yahoo.com>
2018-06-22 13:09:20 -0500

    adding new middleware

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
-const {ObjectID} = require('mongodb')
+const {ObjectID} = require('mongodb');
+const {authenticate} = require('./middleware/authenticate')
+
 
 
 var app = express();
@@ -120,6 +122,13 @@ app.post('/users', (req, res) => {
 });
 
 
+
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+})
 
 
 app.listen(port, () => {
